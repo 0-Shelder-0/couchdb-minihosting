ad58106c9e29e5cb4236d90680a42c6b25e8a2e7
Vladislav Piligrimov <vladpiligrimov@gmail.com>
2026-02-08 10:49:44 +0300

    fix: Fixed couchdb configure

--- install.sh
+++ install.sh
@@ -62,21 +62,23 @@ echo "Setting up CouchDB…"
 # Make sure docker hasn't stolen deploy’s directory
 chown deploy /home/deploy/web
 
+
 # Configure cluster as single node
 curl -X POST "${BASE_URL}/_cluster_setup" \
   -H "Content-Type: application/json" \
-  -d "{\"action\":\"enable_single_node\",\"username\":\"${COUCHDB_USER}\",\"password\":\"${COUCHDB_PASSWORD}\",\"bind_address\":\"0.0.0.0\",\"port\":5984,\"singlenode\":true}"  \
-  --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}"
+  -d "{\"action\":\"enable_single_node\",\"username\":\"${COUCHDB_USER}\",\"password\":\"${COUCHDB_PASS}\",\"bind_address\":\"0.0.0.0\",\"port\":5984,\"singlenode\":true}"  \
+  --user "${COUCHDB_USER}:${COUCHDB_PASS}"
 
 # Configure other parameters
-curl -X PUT "${CONFIG_URL}/chttpd/require_valid_user" -H "Content-Type: application/json" -d '"true"' --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}"
-curl -X PUT "${CONFIG_URL}/chttpd/max_http_request_size" -H "Content-Type: application/json" -d '"4294967296"' --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}"
-curl -X PUT "${CONFIG_URL}/couchdb/max_document_size" -H "Content-Type: application/json" -d '"50000000"' --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}"
+curl -X PUT "${CONFIG_URL}/chttpd/require_valid_user" -H "Content-Type: application/json" -d '"true"' --user "${COUCHDB_USER}:${COUCHDB_PASS}"
+curl -X PUT "${CONFIG_URL}/chttpd/max_http_request_size" -H "Content-Type: application/json" -d '"4294967296"' --user "${COUCHDB_USER}:${COUCHDB_PASS}"
+curl -X PUT "${CONFIG_URL}/couchdb/max_document_size" -H "Content-Type: application/json" -d '"50000000"' --user "${COUCHDB_USER}:${COUCHDB_PASS}"
 
 # Create system databases
-curl -X PUT ${BASE_URL}/_users
-curl -X PUT ${BASE_URL}/_replicator
-curl -X PUT ${BASE_URL}/_global_changes
+curl -X PUT "${BASE_URL}/_users"
+curl -X PUT "${BASE_URL}/_replicator"
+curl -X PUT "${BASE_URL}/_global_changes"
+
 
 if [ "$ENABLE_CORS" = "true" ]; then
   curl -X PUT ${CONFIG_URL}/chttpd/enable_cors -d '"true"'
